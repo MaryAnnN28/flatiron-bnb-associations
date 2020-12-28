785e50163d43d4f3456e8546df9c0ac668cfd656
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-12-28 11:51:06 -0500

    commit tree 81d72173ea119162de94120d0d653ca4b592eeb4

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
 
   resources :listings
 
-  resources :neighborhoods
-
-  resources :cities
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
